003f14ef01ed86bea30e3a5731684e064c9958f7
창민 <ckd29672@naver.com>
2023-05-06 23:08:54 +0900

    splash screen 프로젝트

---
+++ changmin/changmin_flutter_project/lib/project/spashscreen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Container(
+          decoration: BoxDecoration(color: Color(0xFFea342d)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 400,
+                child: Container(
+                    margin: EdgeInsets.all(50),
+                    child: Image.asset('assets/logo.png')),
+              ),
+              CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(Colors.black),
+              )
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
