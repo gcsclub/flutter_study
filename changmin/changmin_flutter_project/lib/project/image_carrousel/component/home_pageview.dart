08df9c0a6f22d4fdc3c2ac2a89a1ea32cc48416b
창민 <ckd29672@naver.com>
2023-05-08 02:40:22 +0900

    image_carrousel 프로젝트 완성

--- changmin/changmin_flutter_project/lib/project/image_carrousel/component/home_pageview.dart
+++ changmin/changmin_flutter_project/lib/project/image_carrousel/component/home_pageview.dart
@@ -4,14 +4,17 @@ import 'package:flutter/material.dart';
 
 class HomePageView {
   List<String> image_path_list;
+  PageController pageController;
 
   HomePageView(
       {required List<String> image_path_list,
       required PageController pageController})
-      : this.image_path_list = image_path_list;
+      : this.image_path_list = image_path_list,
+        this.pageController = pageController;
 
   PageView createPageView() {
     return PageView(
+      controller: pageController,
       children: image_path_list
           .map((e) => Image.asset(
                 e,
